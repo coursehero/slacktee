cc8638601c331a86c6c3f12411c380b55be6d587
Sam Duke <samskiter@users.noreply.github.com>
2015-01-16 12:12:02 +0000

    Auto-parse links (fixes Issue #9)
    see https://api.slack.com/docs/formatting

--- slacktee.sh
+++ slacktee.sh
@@ -53,7 +53,7 @@ function send_message(){
     message=$1
     if [[ $message != "" ]]; then
         escapedText=$(echo \`\`\`$message\`\`\` | sed 's/"/\"/g' | sed "s/'/\'/g" )
-        json="{\"channel\": \"#$channel\", \"username\": \"$username\", \"text\": \"$escapedText\", \"icon_emoji\": \":$icon:\"}"
+        json="{\"channel\": \"#$channel\", \"username\": \"$username\", \"text\": \"$escapedText\", \"icon_emoji\": \":$icon:\", \"parse\": \"full\"}"
         post_result=`curl -X POST --data-urlencode "payload=$json" $webhook_url 2>/dev/null`
     fi
 }
