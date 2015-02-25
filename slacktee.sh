59f5612cd74d0e565113edf4479822a062886099
Chuck Kato <ckato@coursehero.com>
2015-02-25 11:36:59 -0800

    Bugfix: Escape double/single quote correctly

--- slacktee.sh
+++ slacktee.sh
@@ -58,7 +58,7 @@ function show_help(){
 function send_message(){
     message=$1
     if [[ $message != "" ]]; then
-        escapedText=$(echo $textWrapper$message$textWrapper | sed 's/"/\"/g' | sed "s/'/\'/g" )
+        escapedText=$(echo $textWrapper$message$textWrapper | sed 's/"/\\"/g' | sed "s/'/\\'/g" )
         json="{\"channel\": \"#$channel\", \"username\": \"$username\", \"text\": \"$escapedText\", \"icon_emoji\": \":$icon:\" $parseMode}"
         post_result=`curl -X POST --data-urlencode "payload=$json" $webhook_url 2>/dev/null`
     fi
