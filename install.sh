0d184313e7434294bc8ba0d53eac459282daa898
Laurent Arnoud <laurent.arnoud@td-berlin.com>
2015-10-16 14:46:22 +0200

    Install improvements

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 install_path=/usr/local/bin
-if [[ $# -ne 0 ]]; then
+slacktee_script="slacktee.sh"
+
+if [ $# -ne 0 ]; then
     install_path=$1
 fi
 script_dir=$( cd $(dirname $0); pwd -P )
 
 # Copy slacktee.sh to /usr/local/bin 
-cp $script_dir/slacktee.sh $install_path
+cp "$script_dir/$slacktee_script" "$install_path"
 
 # Set execute permission
-chmod +x $install_path/slacktee.sh
+chmod +x "$install_path/$slacktee_script"
 
-echo "slacktee.sh has been installed to $install_path"
+echo "$slacktee_script has been installed to $install_path"
 
 # Execute slacktee.sh with --setup option
-$install_path/slacktee.sh --setup
+"$install_path/$slacktee_script" --setup
